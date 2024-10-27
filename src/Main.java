afab00b97defa825e2876300b09ecfec0902e2ef
alfred <alfred.withers@seznam.cz>
2024-10-27 14:36:59 +0100

    Drawing line works even with overlapping lines, there is an issue with the stat panel not being repainted properly.  - Might add a devInfoPanel to show details of what is going on and display things like overlapping lines, vertecies and other useful information.  - I thought injecting everything though set methods would be more readable instead of using contructor but it didnt, I think I will change that. - Now working on creating polygons by drawing lines that intersect, if two or more lines are connected but not closed off, then they will form a connected line object, if they are closed of they will form a polygon object. If a line passes twice through the same connected line object, it will form a polygon, if it passes through a polygon then it will form 2 new polygons from the old one.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import panel.MainPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        MainPanel mainPanel = new MainPanel(800, 600);
+
+    }
+}
