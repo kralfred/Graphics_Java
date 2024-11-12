5f4a2f14233bf785d7229896fbc0c33be70b6f68
alfred <alfred.withers@seznam.cz>
2024-11-12 16:15:44 +0100

    working on spliting lines with the split method. Should create new lines and vertecies from lines.

---
+++ src/object/ConnectedPoint.java
@@ -0,0 +1,38 @@
+package object;
+
+import java.util.ArrayList;
+
+public class ConnectedPoint {
+
+
+    ArrayList<Line> connectedLines;
+    Point currentPoint;
+
+    public ConnectedPoint(ArrayList<Line> connectedLines, Point currentPoint) {
+        this.currentPoint = currentPoint;
+        this.connectedLines = connectedLines;
+    }
+
+    public void addNeighbour(Line line) {
+       connectedLines.add(line);
+    }
+
+    public void setConnectedLines(){
+
+    }
+    public void addConnectedLine(Line line){
+        connectedLines.add(line);
+    }
+    public void removeConnectedLine(Line line){
+        connectedLines.remove(line);
+    }
+
+    public Point getCurrentPoint() {
+        return currentPoint;
+    }
+    public ArrayList<Line> getConnectedLines() {
+        return connectedLines;
+    }
+
+
+}
